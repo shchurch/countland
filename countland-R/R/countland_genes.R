7d9d039023a4f124e22ce84eb5884f05efc13b2e
shchurch <samuelchurch@samuels-mbp-2.lan>
2022-05-23 16:15:01 -0600

    updating subsample and count_index functionality

--- countland-R/R/countland_genes.R
+++ countland-R/R/countland_genes.R
@@ -33,24 +33,73 @@ listCols<-function(m){
 #' Subsample cells to a standard number of counts by randomly sampling observations without replacement.
 #'
 #' @param C countland object
-#' @param n_counts number of counts to subsample, must be larger than min total counts per cell
+#' @param gene_counts maximum total counts for genes
+#' @param cell_counts sequencing depth for all cells, or "min" to use the minimum cell total
 #'
 #' @return countland object with slot `subsample`
 #' @export
-Subsample <- function(C,n_counts){
-    lms <- listCols(C@counts)
-    lis <- split(C@counts@i, findInterval(seq_len(Matrix::nnzero(C@counts)), C@counts@p, left.open=T))
+Subsample <- function(C,gene_counts=NA,cell_counts=NA){
+    stopifnot("must choose either gene_counts or cell_counts"= !(is.na(gene_counts) && is.na(cell_counts)))
 
-    v <- lapply(seq_len(C@counts@Dim[[2]]),function(x){SubsampleCol(lms[[x]],lis[[x]],x,n_counts)})
-    vd <- do.call(rbind,lapply(v,as.data.frame))
-    vt <- as(C@counts,"dgTMatrix")
-    vt@i <- vd$i
-    vt@j <- vd$j
-    vt@x <- vd$x
-    vc <- as(vt,"dgCMatrix")
+    if(!is.na(gene_counts)) {
+        # invert the matrix to subsample gene vectors
+        t_counts <- Matrix::t(C@counts)
 
-    C@subsample <- vc
+        # get gene vectors from dgC matrix
+        gene_vectors <- listCols(t_counts)
+        gene_row_positions <- split(t_counts@i, findInterval(seq_len(Matrix::nnzero(t_counts)), t_counts@p, left.open=T))
 
+        # set number to subsample to
+        gene_total_counts <- sapply(gene_vectors,sum)
+        above_threshold <- gene_total_counts > gene_counts
+        gene_total_counts[above_threshold] <- gene_counts
+        print(paste0("subsampling ",sum(above_threshold)," genes to a max total counts of ",gene_counts))
+
+        # subsample genes
+        new_gene_vectors <- lapply(seq_len(t_counts@Dim[[2]]),
+                                   function(x){SubsampleCol(gene_vectors[[x]],
+                                                            gene_row_positions[[x]],
+                                                            x,
+                                                            gene_total_counts[[x]])})
+
+        # build dgT matrix, then convert to dgC
+        new_gene_df <- do.call(rbind,lapply(new_gene_vectors,as.data.frame))
+        new_gene_mat <- as(t_counts,"dgTMatrix")
+        new_gene_mat@i <- new_gene_df$i; new_gene_mat@j <- new_gene_df$j; new_gene_mat@x <- new_gene_df$x
+        subsample <- as(Matrix::t(new_gene_mat),"dgCMatrix")
+    }
+
+    if(!is.na(cell_counts)) {
+        # get the matrix
+        if(is.na(gene_counts)){
+            counts <- C@counts
+        } else {
+            counts <- subsample
+        }
+
+        # set number to subsample to
+        if(cell_counts == "min") {
+            cell_counts <- min(apply(counts,2,sum))
+        }
+        print(paste0("subsampling all cells to a standard sequencing depth of ",cell_counts))
+
+        # subsample cells
+        cell_vectors <- listCols(counts)
+        cell_row_positions <- split(counts@i, findInterval(seq_len(Matrix::nnzero(counts)), counts@p, left.open=T))
+        new_cell_vectors <- lapply(seq_len(counts@Dim[[2]]),
+                                   function(x){SubsampleCol(cell_vectors[[x]],
+                                                            cell_row_positions[[x]],
+                                                            x,
+                                                            cell_counts)})
+
+        # build dgT matrix, then convert to dgC
+        new_cell_df <- do.call(rbind,lapply(new_cell_vectors,as.data.frame))
+        new_cell_mat <- as(counts,"dgTMatrix")
+        new_cell_mat@i <- new_cell_df$i; new_cell_mat@j <- new_cell_df$j; new_cell_mat@x <- new_cell_df$x
+        subsample <- as(new_cell_mat,"dgCMatrix")
+    }
+
+    C@subsample <- subsample
     return(C)
 }
 
@@ -60,10 +109,10 @@ Subsample <- function(C,n_counts){
 #'
 #' @return count index = largest n where n cells have >= n counts
 CountIndex<-function(lm){
-    tb <- table(lm)
-    namestb <- as.numeric(names(tb))
-    if(any(tb >= as.numeric(names(tb)))) {
-        return(max(namestb[tb >= as.numeric(names(tb))]))
+    vals_geq <- sapply(unique(lm),function(x){sum(lm >= x)})
+    values <- unique(lm)
+    if(any(vals_geq >= values)) {
+        return(max(values[vals_geq >= values]))
     } else {
         return(0)
     }
